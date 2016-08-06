89f66e6e96d40e072bdc198900f0cbc58aa763ed
Leonardo Larocca <leonardo_larocca@hotmail.com>
2016-08-06 20:48:03 -0300

    Adicionado funções: Negativo, Níveis RGB

--- js/aprimoramentos.js
+++ js/aprimoramentos.js
@@ -73,6 +73,26 @@ function sepia(){
 	setPixels(pixels);
 }
 
+function negative(){
+	var pixels = getPixels();
+	for(var i = 0; i < pixels.data.length; i += 4){
+		pixels.data[i] = 255 - pixels.data[i];
+		pixels.data[i+1] = 255 - pixels.data[i+1];
+		pixels.data[i+2] = 255 - pixels.data[i+2];
+	}
+	setPixels(pixels);
+}
+
+function RGB_Levels(r,g,b){
+	var pixels = getPixels();
+	var Olho = new Sensibilidade();
+	for (var i = 0; i < pixels.data.length; i += 4){
+	    pixels.data[i] += r ;
+	    pixels.data[i+1] += g;
+	    pixels.data[i+2] += b;
+	}
+	setPixels(pixels);
+}
 
 
 

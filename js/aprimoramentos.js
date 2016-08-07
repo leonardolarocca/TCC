ce5463728deda7e0e936a340afd153903b915688
Leonardo Larocca <leonardo_larocca@hotmail.com>
2016-08-07 01:50:09 -0300

    Adicionado: Filtro Noise

--- js/aprimoramentos.js
+++ js/aprimoramentos.js
@@ -49,15 +49,19 @@ function brightness(ajuste){
 }
 
 function saturation(ajuste){
-	var pixels = getPixels();
-	var Olho = new Sensibilidade();
-	for(var i = 0; i < pixels.data.length; i += 4){
-		var p = Math.sqrt( ((pixels.data[i]^2) * Olho.r) + ((pixels.data[i+1]^2) * Olho.g) + ((pixels.data[i+2]^2) * Olho.b));
-		pixels.data[i] = p + (pixels.data[i]-p) * ajuste;
-		pixels.data[i+1] = p + (pixels.data[i+1]-p) * ajuste;
-		pixels.data[i+2] = p + (pixels.data[i+2]-p) * ajuste;
+	if(ajuste <= 0){
+		grayScale();
+	}else{
+		var pixels = getPixels();
+		var Olho = new Sensibilidade();
+		for(var i = 0; i < pixels.data.length; i += 4){
+			var p = Math.sqrt( ((pixels.data[i]^2) * Olho.r) + ((pixels.data[i+1]^2) * Olho.g) + ((pixels.data[i+2]^2) * Olho.b));
+			pixels.data[i] = p + (pixels.data[i]-p) * ajuste;
+			pixels.data[i+1] = p + (pixels.data[i+1]-p) * ajuste;
+			pixels.data[i+2] = p + (pixels.data[i+2]-p) * ajuste;
+		}
+		setPixels(pixels);
 	}
-	setPixels(pixels);
 }
 
 function sepia(){
@@ -90,6 +94,26 @@ function RGB_Levels(r,g,b){
 	    pixels.data[i] += r ;
 	    pixels.data[i+1] += g;
 	    pixels.data[i+2] += b;
+	}
+	setPixels(pixels);
+}
+
+function noise(ajuste){
+	var pixels = getPixels();
+	for(var i = 0; i < pixels.data.length; i += 4){
+		var num = (0.5 - Math.random()) * ajuste;
+		pixels.data[i] = (pixels.data[i] + num);
+		pixels.data[i+1] = (pixels.data[i+1] + num);
+		pixels.data[i+2] = (pixels.data[i+2] + num);
+	}
+	setPixels(pixels);
+}
+
+function sharpen(ajuste){
+	var pixels = getPixels();
+	
+	for(var i = 0; i < pixels.data.length; i += 4){
+
 	}
 	setPixels(pixels);
 }
